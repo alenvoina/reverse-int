e56a8483d3659ef54099ad08da0257b8ac0dae37
alenvoina <70748371+alenvoina@users.noreply.github.com>
2020-10-19 11:45:49 +0300

    complete task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let finish = String(n).split('')
+  finish = finish.reverse()
+
+  let itog = ''
+
+  itog = itog.concat(finish[0],finish[1],finish[2])
+  return itog
 }
